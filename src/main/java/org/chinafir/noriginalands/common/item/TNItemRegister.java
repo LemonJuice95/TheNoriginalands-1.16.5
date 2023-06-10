a4fd2824239e25aabdd85f05333a93cbd1c52d9a
Administrator <1582017385@qq.com>
2023-06-10 12:25:02 +0800

    Added Raw Iron and Raw Gold

---
+++ src/main/java/org/chinafir/noriginalands/common/item/TNItemRegister.java
@@ -0,0 +1,15 @@
+package org.chinafir.noriginalands.common.item;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.chinafir.noriginalands.utils.Reference;
+
+public class TNItemRegister {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MODID);
+
+    public static final RegistryObject<Item> RAW_IRON = ITEMS.register("raw_iron", () -> new TNItembase(new Item.Properties().tab(TNGroupRegister.tngroup)));
+    public static final RegistryObject<Item> RAW_GOLD = ITEMS.register("raw_gold", () -> new TNItembase(new Item.Properties().tab(TNGroupRegister.tngroup)));
+
+}
