78c31977240ef24ccaa731fd68b699de337b5f48
Administrator <1582017385@qq.com>
2023-06-12 23:55:50 +0800

    Added Indigo Steel Ingot

--- src/main/java/org/chinafir/noriginalands/common/item/TNItemRegister.java
+++ src/main/java/org/chinafir/noriginalands/common/item/TNItemRegister.java
@@ -9,8 +9,8 @@ import org.chinafir.noriginalands.utils.Reference;
 public class TNItemRegister {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MODID);
 
-    public static final RegistryObject<Item> RAW_IRON = ITEMS.register("raw_iron", () -> new TNItembase(new Item.Properties().tab(TNGroupRegister.tngroup)));
-    public static final RegistryObject<Item> RAW_GOLD = ITEMS.register("raw_gold", () -> new TNItembase(new Item.Properties().tab(TNGroupRegister.tngroup)));
+    public static final RegistryObject<Item> RAW_IRON = ITEMS.register("raw_iron", () -> new TNItemBase(new Item.Properties().tab(TNGroupRegister.tngroup)));
+    public static final RegistryObject<Item> RAW_GOLD = ITEMS.register("raw_gold", () -> new TNItemBase(new Item.Properties().tab(TNGroupRegister.tngroup)));
 
-    public static final RegistryObject<Item> INDIGO_STEEL_INGOT = ITEMS.register("indigo_steel_ingot", () -> new TNItembase(new Item.Properties().tab(TNGroupRegister.tngroup)));
+    public static final RegistryObject<Item> INDIGO_STEEL_INGOT = ITEMS.register("indigo_steel_ingot", () -> new TNItemBase(new Item.Properties().tab(TNGroupRegister.tngroup)));
 }
