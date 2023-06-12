5f2db22ffce587c2cd8dc60edcd5d4770470d2ae
Administrator <1582017385@qq.com>
2023-06-12 23:56:03 +0800

    Initial Commit

--- src/main/java/org/chinafir/noriginalands/common/item/TNItemBase.java
+++ src/main/java/org/chinafir/noriginalands/common/item/TNItemBase.java
@@ -2,8 +2,8 @@ package org.chinafir.noriginalands.common.item;
 
 import net.minecraft.item.Item;
 
-public class TNItembase extends Item {
-    public TNItembase(Item.Properties properties) {
+public class TNItemBase extends Item {
+    public TNItemBase(Item.Properties properties) {
         super(properties);
     }
 }
